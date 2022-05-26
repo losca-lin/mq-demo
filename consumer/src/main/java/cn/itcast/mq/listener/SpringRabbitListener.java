84301f15af897e72b2aa8d49fcb00a5575d55751
losca <783840358@qq.com>
2022-05-26 15:52:52 +0800

    3种发布订阅模式 消息转换器

--- consumer/src/main/java/cn/itcast/mq/listener/SpringRabbitListener.java
+++ consumer/src/main/java/cn/itcast/mq/listener/SpringRabbitListener.java
@@ -1,7 +1,13 @@
 package cn.itcast.mq.listener;
 
+import org.springframework.amqp.core.ExchangeTypes;
+import org.springframework.amqp.rabbit.annotation.Exchange;
+import org.springframework.amqp.rabbit.annotation.Queue;
+import org.springframework.amqp.rabbit.annotation.QueueBinding;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
+
+import java.util.Map;
 
 /**
  * @author Losca
@@ -32,5 +38,46 @@ public class SpringRabbitListener {
     public void fanoutQueue12(String msg) throws InterruptedException {
         System.out.println("=======================");
         System.err.println("fanout:queue2："+msg);
+    }
+
+    @RabbitListener(bindings = @QueueBinding(
+            value = @Queue(name = "direct.queue1"),
+            exchange = @Exchange(name = "itcast.direct", type = ExchangeTypes.DIRECT),
+            key = {"red", "blue"}
+    ))
+    public void listenDirectQueue1(String msg){
+        System.out.println("消费者接收到direct.queue1的消息：【" + msg + "】");
+    }
+
+    @RabbitListener(bindings = @QueueBinding(
+            value = @Queue(name = "direct.queue2"),
+            exchange = @Exchange(name = "itcast.direct", type = ExchangeTypes.DIRECT),
+            key = {"red", "green"}
+    ))
+    public void listenDirectQueue2(String msg){
+        System.out.println("消费者接收到direct.queue2的消息：【" + msg + "】");
+    }
+
+    @RabbitListener(bindings = @QueueBinding(
+            value = @Queue(name = "topic.queue1"),
+            exchange = @Exchange(name = "itcast.topic", type = ExchangeTypes.TOPIC),
+            key = "china.#"
+    ))
+    public void listenTopicQueue1(String msg){
+        System.out.println("消费者接收到topic.queue1的消息：【" + msg + "】");
+    }
+
+    @RabbitListener(bindings = @QueueBinding(
+            value = @Queue(name = "topic.queue2"),
+            exchange = @Exchange(name = "itcast.topic", type = ExchangeTypes.TOPIC),
+            key = "#.news"
+    ))
+    public void listenTopicQueue2(String msg){
+        System.out.println("消费者接收到topic.queue2的消息：【" + msg + "】");
+    }
+
+    @RabbitListener(queues = "object.queue")
+    public void objectMessage(Map<String,String> message){
+        System.out.println("object的消息"+message);
     }
 }
--- publisher/src/test/java/cn/itcast/mq/spring/SpringAmqpTest.java
+++ publisher/src/test/java/cn/itcast/mq/spring/SpringAmqpTest.java
@@ -7,6 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author Losca
  * @date 2022/5/26 11:30
@@ -34,5 +37,35 @@ public class SpringAmqpTest {
         // 消息
         String message = "hello, everyone!";
         rabbitTemplate.convertAndSend(exchangeName, "", message);
+    }
+
+    @Test
+    public void testSendDirectExchange() {
+        // 队列名称
+        String exchangeName = "itcast.direct";
+        // 消息
+        String message = "hello, red!";
+        rabbitTemplate.convertAndSend(exchangeName, "red", message);
+    }
+
+    @Test
+    public void testSendTopicExchange() {
+        // 交换机名称
+        String exchangeName = "itcast.topic";
+        // 消息
+        String message = "喜报！孙悟空大战哥斯拉，胜!";
+        // 发送消息
+        rabbitTemplate.convertAndSend(exchangeName, "china.weather", message);
+    }
+
+    @Test
+    public void testSendObject() {
+        Map<String,String> map = new HashMap<>();
+        // 消息
+        String message = "喜报！孙悟空大战哥斯拉，胜!";
+        map.put("msg", message);
+        map.put("statu", "err");
+        // 发送消息
+        rabbitTemplate.convertAndSend( "object.queue", map);
     }
 }
